95fb7a05724918dedbef1600478865640a51ea7d
Pain-Dzn <edsonpain29@gmail.com>
2025-12-08 09:40:53 +0200

    Um commit que nem sei o que eu fiz

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-    historyApiFallback: true
+    open: true
   },
   build: {
     outDir: 'dist',
